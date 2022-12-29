2223be72ea3ae63cf98d45add3057bdc8bd373d5
William Santos <willsantos.edf@gmail.com>
2022-12-29 11:29:13 -0300

    Adicionando tabelas no banco de dados.

--- petshop/settings.py
+++ petshop/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base',
+    'base'
 ]
+
+AUTH_USER_MODELS = 'base.ReservaServico'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
