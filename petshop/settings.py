ab01914d2141477f8598d5a50716f8f5330744cc
William Santos <willsantos.edf@gmail.com>
2023-01-01 21:27:17 -0300

    Ajustando html.

--- petshop/settings.py
+++ petshop/settings.py
@@ -110,7 +110,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
