d70e0d2c9be71ba239e6495424de83ed6a00f538
William Santos <willsantos.edf@gmail.com>
2023-01-14 10:24:16 -0300

    Criando restriçoes no campo data.

--- reserva/forms.py
+++ reserva/forms.py
@@ -11,13 +11,13 @@ class ReservaForm(forms.ModelForm):
             raise forms.ValidationError('Não é possivel realizar uma reserva para o passado!')
         return data
 
-    """ def clean_turno(self):
+    def clean_turno(self):
         reserva_tarde = Reserva.objects.filter(turno='tarde').count()
         quant_max = 10
         if reserva_tarde > quant_max:
             raise forms.ValidationError('Não temos mais vagas para esta data!')
         return reserva_tarde
- """
+
 
     class Meta:
         model = Reserva
